01bbf1d0c9cb8aafbf5f47cd1e19421eebe85861
Auro4037 <104313271+Auro4037@users.noreply.github.com>
2025-05-25 16:34:34 +0530

    Add files via upload

---
+++ Day01/01-assignment1.js
@@ -0,0 +1,16 @@
+//prmitive data types
+
+let firstName = "Auro"
+console.log(typeof firstName);
+
+let companyName="Infosys"
+console.log(typeof companyName);
+
+let phoneNumber = 8992993033
+console.log(typeof phoneNumber);
+
+let isAutomation = true
+console.log(typeof isAutomation);
+
+let hasPlaywright
+console.log(typeof hasPlaywright);
---
+++ Day01/01-get-started.js
@@ -0,0 +1,55 @@
+//numbers
+
+const { type } = require("os");
+
+var phoneNumber = 8838383
+console.log(phoneNumber);
+console.log(typeof phoneNumber);
+
+//string
+ var userName= "Auro"
+ var pwd="sdddd"
+
+ console.log(userName);
+ console.log(pwd);
+ console.log("The datatype of username is " + typeof userName);
+
+ //boolean
+var accountnumverValid= true
+console.log(typeof accountnumverValid);
+
+//undefined
+
+var accountNumber
+console.log(accountNumber);
+console.log(typeof accountNumber);
+
+//null
+
+var middleName =null
+console.log(middleName);
+console.log(typeof middleName);
+
+//using var keyword - both redeclare and reassign
+
+var userName ="aurobinda"
+var userName="auro"//redeclare
+
+userName="auro4037" //reassign
+
+console.log(userName);
+
+//using let keyword - only reassign and not re declare
+
+let userName1 ="aurobinda"
+userName1 ="auro1"
+
+console.log(userName1);
+
+//const keyword - can't be reassigned and redeclare
+
+const id = 4037
+console.log(id)
+
+
+
